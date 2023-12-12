13de12ee25a4ef9fd0acddd73cb526d0d809dbc5
Hafizi Malik <76240694+Fyzie@users.noreply.github.com>
2023-12-12 18:32:38 +0800

    Add files via upload

---
+++ create_image_list_file.py
@@ -0,0 +1,30 @@
+# https://github.com/computervisioneng/train-yolov8-custom-dataset-step-by-step-guide/tree/master/prepare_data
+
+import os
+
+door_id = '/m/02dgv'
+
+train_bboxes_filename = os.path.join('.', 'oidv6-train-annotations-bbox.csv')
+validation_bboxes_filename = os.path.join('.', 'validation-annotations-bbox.csv')
+test_bboxes_filename = os.path.join('.', 'test-annotations-bbox.csv')
+
+image_list_file_path = os.path.join('.', 'image_list_file') # saved file with all desired image ids
+
+# limit = 100 # to limit number of desired images
+image_list_file_list = []
+for j, filename in enumerate([train_bboxes_filename, validation_bboxes_filename, test_bboxes_filename]):
+    ## to limit number of desired images
+    # if j == limit:
+    #     break
+    print(filename)
+    with open(filename, 'r') as f:
+        line = f.readline()
+        while len(line) != 0:
+            id, _, class_name, _, x1, x2, y1, y2, _, _, _, _, _ = line.split(',')[:13]
+            if class_name in [door_id] and id not in image_list_file_list:
+                image_list_file_list.append(id)
+                with open(image_list_file_path, 'a') as fw:
+                    fw.write('{}/{}\n'.format(['train', 'validation', 'test'][j], id))
+            line = f.readline()
+
+        f.close()
