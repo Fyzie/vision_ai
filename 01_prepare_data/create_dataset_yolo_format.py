7db58ae82e0c61c92cc8eb0ac8e02a419dbe0ecb
Hafizi Malik <76240694+Fyzie@users.noreply.github.com>
2023-12-13 12:34:40 +0800

    Update create_dataset_yolo_format.py

--- 01_prepare_data/create_dataset_yolo_format.py
+++ 01_prepare_data/create_dataset_yolo_format.py
@@ -3,11 +3,11 @@
 import os
 import shutil
 
-base_directory = '.'
+base_directory = 'D:\Pycharm Projects\YPPB Projects\yolov8'
 
 DATA_ALL_DIR = os.path.join(base_directory, '$DOWNLOAD_FOLDER')
 
-DATA_OUT_DIR = os.path.join(base_directory, 'data')
+DATA_OUT_DIR = os.path.join(base_directory, 'data_2')
 
 for set_ in ['train', 'validation', 'test']:
     for dir_ in [os.path.join(DATA_OUT_DIR, 'images', set_),
@@ -33,6 +33,7 @@ for j, filename in enumerate([train_bboxes_filename, validation_bboxes_filename,
 
         ############################### if limiter was used before ######################
         num_id = 0 
+        num = 0
 
         while len(line) != 0:
             id, _, class_name, _, x1, x2, y1, y2, _, _, _, _, _ = line.split(',')[:13]
@@ -41,14 +42,14 @@ for j, filename in enumerate([train_bboxes_filename, validation_bboxes_filename,
                     shutil.copy(os.path.join(DATA_ALL_DIR, '{}.jpg'.format(id)),
                                 os.path.join(DATA_OUT_DIR, 'images', set_, '{}.jpg'.format(id)))
                 with open(os.path.join(DATA_OUT_DIR, 'labels', set_, '{}.txt'.format(id)), 'a') as f_ann:
-                    # class_id, xc, yx, w, h
                     x1, x2, y1, y2 = [float(j) for j in [x1, x2, y1, y2]]
-                    xc = (x1 + x2) / 2
-                    yc = (y1 + y2) / 2
+
+                    x = x1
+                    y = y1
                     w = x2 - x1
                     h = y2 - y1
 
-                    f_ann.write('0 {} {} {} {}\n'.format(xc, yc, w, h))
+                    f_ann.write('0 {} {} {} {}\n'.format(x, y, w, h))
                     f_ann.close()
 
                 ####################### if limiter was used before ##############################
@@ -56,5 +57,6 @@ for j, filename in enumerate([train_bboxes_filename, validation_bboxes_filename,
                 if num_id == limit[j]:
                     break
                 #################################################################################
+            num += 1
 
             line = f.readline()
