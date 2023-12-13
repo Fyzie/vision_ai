aedebdd8e08b7b4c419cff9da4de3815f1c086f0
Hafizi Malik <76240694+Fyzie@users.noreply.github.com>
2023-12-13 19:47:06 +0800

    Add files via upload

---
+++ 02_object_detection/predict_video.py
@@ -0,0 +1,44 @@
+import os
+
+from ultralytics import YOLO
+import cv2
+
+video_path = 'door_showroom.mp4'
+video_path_out = '{}_out.mp4'.format(video_path)
+
+cap = cv2.VideoCapture(video_path)
+ret, frame = cap.read()
+H, W, _ = frame.shape
+out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
+
+model_path = os.path.join('results', 'train', 'weights', 'best.pt')
+
+# Load a model
+model = YOLO(model_path)  # load a custom model
+
+threshold = 0.5
+
+while ret:
+
+    results = model(frame)[0]
+
+    for result in results.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = result
+
+        if score > threshold:
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+            cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+
+    cv2.imshow('Video Monitoring', frame)
+    out.write(frame)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):  # Press 'q' to exit the loop and close the window
+        break
+
+    ret, frame = cap.read()
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
