e004b2c1d37d78fb4310bdcd6450101bbe68b928
Hafizi Malik <76240694+Fyzie@users.noreply.github.com>
2023-12-13 15:43:49 +0800

    Update and rename check_ids_amount.py to check_image_amount.py

--- 01_prepare_data/check_image_amount.py
+++ 01_prepare_data/check_image_amount.py
@@ -10,13 +10,15 @@ filenames = [train_bboxes_filename, validation_bboxes_filename, test_bboxes_file
 
 num_data = [0]*len(filenames)
 set_ = ['train', 'val', 'test']
+image_list = []
 for j, filename in enumerate(filenames):
     print(filename)
     with open(filename, 'r') as f:
         line = f.readline()
         while len(line) != 0:
             id, _, class_name, _, x1, x2, y1, y2, _, _, _, _, _ = line.split(',')[:13]
-            if class_name in [door_id]:
+            if class_name in [door_id] and id not in image_list:
+                image_list.append(id)
                 num_data[j] += 1
 
             line = f.readline()
