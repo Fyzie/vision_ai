553db3c53daa9336b65431305976545630c4738b
Hafizi Malik <76240694+Fyzie@users.noreply.github.com>
2023-12-13 15:44:14 +0800

    Update create_dataset_yolo_format.py

--- 01_prepare_data/create_dataset_yolo_format.py
+++ 01_prepare_data/create_dataset_yolo_format.py
@@ -2,12 +2,13 @@
 
 import os
 import shutil
+import time
 
 base_directory = 'D:\Pycharm Projects\YPPB Projects\yolov8'
 
 DATA_ALL_DIR = os.path.join(base_directory, '$DOWNLOAD_FOLDER')
 
-DATA_OUT_DIR = os.path.join(base_directory, 'data')
+DATA_OUT_DIR = os.path.join(base_directory, 'data_2')
 
 for set_ in ['train', 'validation', 'test']:
     for dir_ in [os.path.join(DATA_OUT_DIR, 'images', set_),
@@ -22,8 +23,6 @@ train_bboxes_filename = os.path.join(base_directory, 'oidv6-train-annotations-bb
 validation_bboxes_filename = os.path.join(base_directory, 'validation-annotations-bbox.csv')
 test_bboxes_filename = os.path.join(base_directory, 'test-annotations-bbox.csv')
 
-########## if limiter was used before (make sure the numbers are same) ###########
-limit = [300, 90, 30] 
 
 for j, filename in enumerate([train_bboxes_filename, validation_bboxes_filename, test_bboxes_filename]):
     set_ = ['train', 'validation', 'test'][j]
@@ -31,16 +30,15 @@ for j, filename in enumerate([train_bboxes_filename, validation_bboxes_filename,
     with open(filename, 'r') as f:
         line = f.readline()
 
-        ############################### if limiter was used before ######################
-        num_id = 0 
-
         while len(line) != 0:
             id, _, class_name, _, x1, x2, y1, y2, _, _, _, _, _ = line.split(',')[:13]
-            if class_name in [door_id]:
-                if not os.path.exists(os.path.join(DATA_OUT_DIR, 'images', set_, '{}.jpg'.format(id))):
-                    shutil.copy(os.path.join(DATA_ALL_DIR, '{}.jpg'.format(id)),
-                                os.path.join(DATA_OUT_DIR, 'images', set_, '{}.jpg'.format(id)))
-                with open(os.path.join(DATA_OUT_DIR, 'labels', set_, '{}.txt'.format(id)), 'a') as f_ann:
+            input_image = os.path.join(DATA_ALL_DIR, '{}.jpg'.format(id))
+            if class_name in [door_id] and os.path.exists(input_image):
+                output_image = os.path.join(DATA_OUT_DIR, 'images', set_, '{}.jpg'.format(id))
+                if not os.path.exists(output_image):
+                    shutil.copy(input_image, output_image)
+                output_label_file = os.path.join(DATA_OUT_DIR, 'labels', set_, '{}.txt'.format(id))
+                with open(output_label_file, 'a') as f_ann:
                     # class_id, xc, yx, w, h
                     x1, x2, y1, y2 = [float(j) for j in [x1, x2, y1, y2]]
 
@@ -52,10 +50,7 @@ for j, filename in enumerate([train_bboxes_filename, validation_bboxes_filename,
                     f_ann.write('0 {} {} {} {}\n'.format(xc, yc, w, h))
                     f_ann.close()
 
-                ####################### if limiter was used before ##############################
-                num_id += 1
-                if num_id == limit[j]:
-                    break
-                #################################################################################
-
             line = f.readline()
+
+        
+    
