0437087cab85bf938400f551d6f5ed3f639a1ed0
Hafizi Malik <76240694+Fyzie@users.noreply.github.com>
2023-12-13 15:48:43 +0800

    Update scratch_object_detection.py

--- 02_object_detection/scratch_object_detection.py
+++ 02_object_detection/scratch_object_detection.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")
 # model = YOLO(".../train/weights/best.pt")  # load a pretrained model 
 
-model.train(data=".../config.yaml", project={saved_dir}, epochs=3) # get your .yaml file, set folder to save model, and declare num of epochs
+model.train(data=".../config.yaml", project='/saved_dir', epochs=3) # get your .yaml file, set folder to save model, and declare num of epochs
